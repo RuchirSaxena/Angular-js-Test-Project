6c4b5b553c239d99117675a7b7e416c59478587e
Ruchir Saxena <ruchirsaxena@gmail.com>
2017-02-22 01:43:10 +0530

    Services

---
+++ Angular Js 1.0/Scripts/Services.js
@@ -0,0 +1,70 @@
+﻿/// <reference path="angular.js" />
+var app = angular.module("MyApp", []);
+
+//Creating Service using Service Method
+app.service("$calculateServiceUsingServiceMethod", function () {
+    this.add = function (value1, value2) {
+        return parseInt(value1) + parseInt(value2);
+    };
+    this.subtract = function (value1, value2) {
+        return value1 - value2
+    };
+
+});
+
+//Creating Service using Factory Method
+app.factory("$calculateServiceUsingFactoryMethod", function () {
+    var fact = {};
+    fact.add = function (value1, value2) {
+        return parseInt(value1) + parseInt(value2);
+    };
+    fact.subtract = function (value1, value2) {
+        return value1 - value2
+    };
+    return fact;
+
+});
+
+//Creating Provider
+app.provider("date", function () {
+    var greet;
+    return {
+        setGreetMessage:function(greetValue){
+            greet = greetValue;
+            console.log(greet);
+        },
+        $get: function () {
+            return {
+                showDate: function () {
+                    var date = new Date();
+                    return "Hi "+greet+" time is"+  date.getHours();
+                }
+            }
+        }
+    }
+});
+
+app.config(function (dateProvider) {
+    var time=dateProvider.$get().showDate();
+    if (time > 0 && time < 12) {
+        dateProvider.setGreetMessage("Good morning");
+    } else {
+        dateProvider.setGreetMessage("Good night");
+    }
+});
+
+
+app.controller('MathController', function ($scope, $calculateServiceUsingServiceMethod, $calculateServiceUsingFactoryMethod,date) {
+    $scope.add = function (a, b) {
+        $scope.result = $calculateServiceUsingServiceMethod.add(a, b);
+       // alert($scope.result);
+    };
+
+    $scope.sub = function (a, b) {
+        $scope.result = $calculateServiceUsingFactoryMethod.subtract(a, b);
+        // alert($scope.result);
+    };
+
+    $scope.currentDate = date.showDate();
+
+});
