00f8aa46cf5b656c3140e450771af48a34b1087a
Ruchir Saxena <ruchirsaxena@gmail.com>
2017-06-19 01:33:51 +0530

    Understanding Custom Directives

--- Angular Js 1.0/Scripts/customDirectives.js
+++ Angular Js 1.0/Scripts/customDirectives.js
@@ -5,8 +5,9 @@ var app = angular.module("myCustomDirective", []);
 app.directive("companyCopyRight", function () {
     var directive = {
         //We can restrict the directive using below values
-        restrict: "EA", //E=Element, A=Attribute , C=CssClass , M=Comments
-        template: "@CopyRight 2016-17 {{title}}"
+        restrict: "AE", //E=Element, A=Attribute , C=CssClass , M=Comments
+        template: "<div>@CopyRight 2016-17 {{title}}</div>",
+        replace: true //This is used to replace the actual Element  with only template Content ie. <user-Control></user-Control>
     };
     return directive;
 });
@@ -14,7 +15,13 @@ app.directive("companyCopyRight", function () {
 //Directive are useful in creating User Controls for our application
 app.directive("userControl", function () {
     var directive = {
-        templateUrl: "DirectiveUserControl.html"
+        templateUrl: "DirectiveUserControl.html",
+        // scope:{}, //For isolating scopes from its parent page control
+        scope: {
+            previewData: "@",//One Way Binding
+            editFeild: "=",//Two Way Binding
+            anyMethod: "&" //To attach controller method to directives
+        }
     }
     return directive;
 });
@@ -22,4 +29,66 @@ app.directive("userControl", function () {
 app.controller("homeCtrl", function ($scope) {
     $scope.title = "Custom Directives";
 
+});
+
+//By Default the parent Controller(personCtrl)
+//and the directive Scopes are same
+app.directive("personInformation", function () {
+    return {
+        restrict: "AE",
+        templateUrl: "PersonDirective.html",
+        //Now if we want to have a seperate scope for 
+        //directive ie isolated scope from its parent controller
+        //then below code is added
+        // scope: {}
+
+        //If we want that some properties of our parent controller is attached 
+        //to our custome directive then we can use below code ,using below
+        //code we can have scope properties from parent control
+        //BUT it will be not attached to the parent control scope object
+        //ie. changes in below property will not make changes int the parent
+        //control scope properties 
+        //One way binding
+        //scope: {
+        //    employeeName: "@"
+        //}
+
+        //Two way binding =>By default binding
+        scope: {
+            employeeName: "="
+        },
+        //compile: function (elem, attrs) {
+        //    console.log("Compiling......");
+        //    console.log(elem.html());
+        //    return {
+        //        pre: function (scope, elements, attrs) {
+        //            console.log("Pre-linking......");
+        //            console.log(elements);
+        //        },
+        //        post: function (scope, elements, attrs) {
+        //            console.log("Post-Linking.....");
+        //            console.log(elements);
+        //        }
+        //    }
+        //}
+
+        //Above code can also be written as below code
+        //coz it is recommended to use post link 
+        link: function (scope, elements, attrs) {
+            console.log('Linking.....');
+            console.log(scope);
+
+            //Whatever conditional code neede to be written
+        }
+    }
+});
+
+app.controller("personCtrl", function ($scope) {
+
+    var person = {
+        name: "Ruchir Saxena",
+        company: "Pyamid IT Consulting",
+        position: "Team Lead"
+    };
+    $scope.person = person;
 });
