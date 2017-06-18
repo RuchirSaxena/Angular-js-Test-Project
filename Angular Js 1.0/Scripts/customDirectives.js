d48a6386408ec9289f9bddc72fb88dc5e0894fe1
Ruchir Saxena <ruchirsaxena@gmail.com>
2017-06-18 09:28:31 +0530

    Understanding Digest Cycle Cros issue Custom Directives

---
+++ Angular Js 1.0/Scripts/customDirectives.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="angular.js" />
+var app = angular.module("myCustomDirective", []);
+
+
+app.directive("companyCopyRight", function () {
+    var directive = {
+        //We can restrict the directive using below values
+        restrict: "EA", //E=Element, A=Attribute , C=CssClass , M=Comments
+        template: "@CopyRight 2016-17 {{title}}"
+    };
+    return directive;
+});
+
+//Directive are useful in creating User Controls for our application
+app.directive("userControl", function () {
+    var directive = {
+        templateUrl: "DirectiveUserControl.html"
+    }
+    return directive;
+});
+
+app.controller("homeCtrl", function ($scope) {
+    $scope.title = "Custom Directives";
+
+});
